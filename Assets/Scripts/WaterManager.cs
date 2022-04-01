6883ffdfc548f1fe0071f82f213f4936e86b6fe0
KevTale <kevin.taleb.dehkordi@gmail.com>
2022-04-02 01:22:48 +0200

    water basic

--- Assets/Scripts/WaterManager.cs
+++ Assets/Scripts/WaterManager.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WaterManager : MonoBehaviour
 {
+    GameObject _tile;
 
-    [SerializeField] private GameObject waterPrefab;
+    GameObject[][] grid;
     void Start()
     {
+        grid = new GameObject[20][];
+        StartCoroutine(Tic());
+    }
 
+    IEnumerator Tic()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+        }
     }
 
     // Update is called once per frame
