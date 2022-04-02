28427ae9f99cb809a65dc561fe2b4dea598f787d
MaurreKiTu <alexandre.sabouni@gmail.com>
2022-04-02 05:44:29 +0200

    fixes

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -129,7 +129,7 @@ public class Tile : MonoBehaviour
         // _renderer.material.color = color;
         for(int i = 0; i<transform.childCount; i++)
         {
-            //transform.GetChild(i).gameObject.SetActive(i == stateId);
+            transform.GetChild(i).gameObject.SetActive(i == stateId);
         }
     }
 }
