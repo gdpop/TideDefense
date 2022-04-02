083ba1d26ccf167e169bbc8f834bfd2280df1d8c
TiniPot <valentin.piton1@gmail.com>
2022-04-02 05:26:40 +0200

    New tile state

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -74,6 +74,31 @@ public class Tile : MonoBehaviour
         _isClicked = active;
         //Call SetColor using the shader property name "_Color" and setting the color to red
         ChangeColor(active? Color.green: _isHovered? Color.red: initColor);
+
+        OnTileClick();
+    }
+
+    private void OnTileClick()
+    {
+        switch (State)
+        {
+            case TileState.Sand:
+                break;
+            case TileState.WetSand:
+                break;
+            case TileState.WetMoat:
+                break;
+            case TileState.Moat:
+                break;
+            case TileState.Tower:
+                bool canBuild = SandManager.Instance.RemoveSand(1);
+                if (canBuild)
+                {
+                    State = TileState.Tower;
+                    GridManager.Instance.CurrentGrid.SetTile(XCoord, YCoord, State);
+                }
+                break;
+        }    
     }
 
     private void ChangeColor(Color color)
