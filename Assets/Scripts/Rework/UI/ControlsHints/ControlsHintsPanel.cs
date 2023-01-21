c84e3da9df30d39c3665857146c6051c401ec606
Pierre Mizzi <pierre.mizza@gmail.com>
2023-01-22 01:47:55 +0900

    UIToolkit + UIBuilder

---
+++ Assets/Scripts/Rework/UI/ControlsHints/ControlsHintsPanel.cs
@@ -0,0 +1,17 @@
+namespace TideDefense
+{
+    using UnityEngine;
+
+	// - Idle
+	// Left Click - Grab tool
+
+	// - Shovel
+	// Left Click - Drop Tool
+	// Left Click - Fill bucket
+
+	// - Bucket
+	// Left Click - Drop Tool
+	// Hold Left Click - Build rempart
+
+    public class ControlsHints : MonoBehaviour { }
+}
