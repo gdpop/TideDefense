21a6d53c030b236d687584263492275be4d87db4
Pierre Mizzi <pierre.mizza@gmail.com>
2023-01-31 22:33:41 +0900

    MessageBottle Mesh + Prefabs, WIP UI. WIP clean fortifications

---
+++ Assets/Scripts/Rework/MessageBottle/MessageBottle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TideDefense
+{
+    public class MessageBottle : MonoBehaviour
+    {
+        #region Fields
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
